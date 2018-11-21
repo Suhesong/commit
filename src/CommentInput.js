44c4efd59455ee6118b0e7edb943a4448a0fe79d
suhesong <937445794@qq.com>
2018-11-21 19:32:21 +0800

    评论系统第二次提交

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,50 +1,74 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 
-class CommentInput extends Component{
-    constructor(){
+class CommentInput extends Component {
+    constructor() {
         super()
-        this.state={
-            username:'',
-            content:''
+        this.state = {
+            username: '',
+            content: ''
         }
     }
-    handleUsernameChange (event) {
-        this.setState({
-          username: event.target.value
-        })
+    componentDidMount(){
+        this.textarea.focus()
     }
-    handleContentChange (event) {
-        this.setState({
-          content: event.target.value
-        })
+    componentWillMount(){
+        this._loadUsername()
     }
-    handleSubmit(){
-        if(this.props.onSubmit){
-            const {username,content} =this.state
-            this.props.onSubmit({username,content})
+    _loadUsername(){
+        const username = localStorage.getItem('username')
+        if(username){
+            this.setState({
+                username
+            })
         }
-        this.setState({content:''})
     }
-    render(){
+    _saveUsername(username){
+        localStorage.setItem('username',username)
+    }
+    handleUsernameChange(event) {
+        this.setState({
+            username: event.target.value
+        }) 
+    }
+    handleContentChange(event) {
+        this.setState({
+            content: event.target.value 
+        })
+    }
+    handleUsernameBlur(e){
+        this._saveUsername(e.target.value)
+    }
+    handleSubmit() {
+        if (this.props.onSubmit) {
+            const { username, content } = this.state
+            this.props.onSubmit({ username, content ,createdTime:+new Date()})
+        }
+        this.setState({ content: '' })
+    }
+
+    render() {
         return (
             <div className='comment-input'>
                 <div className='comment-field'>
                     <span className='comment-field-name'>用户名：</span>
                     <div className='comment-field-input'>
-                        <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)}></input>
+                        <input 
+                        value={this.state.username}
+                        onBlur={this.handleUsernameBlur.bind(this)} 
+                        onChange={this.handleUsernameChange.bind(this)}></input>
                     </div>
                 </div>
                 <div className='comment-field'>
                     <span className='comment-field-name'>评论内容：</span>
                     <div className='comment-field-input'>
-                    <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)}></textarea>
-                    </div>
-                </div>
-                <div className='comment-field-button'>
-                    <button onClick={this.handleSubmit.bind(this)}>发布</button>
+                        <textarea ref={ (textarea)=> this.textarea=textarea} value={this.state.content} onChange={this.handleContentChange.bind(this)}></textarea>
                 </div>
             </div>
+            <div className='comment-field-button'>
+                <button onClick={this.handleSubmit.bind(this)}>发布</button>
+            </div>
+            </div >
         )
     }
 }
--- src/CommentList.js
+++ src/CommentList.js
@@ -4,10 +4,19 @@ class CommentList extends Component {
     static defaultProps = {
         comments: []
     }
+    handleDeleteComment(index){
+        if(this.props.onDeleteComment){
+            this.props.onDeleteComment(index)
+        }
+    }
     render() {
 
         return (
-            <div >{this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}</div>
+            <div >{this.props.comments.map((comment, i) => <Comment 
+                comment={comment} key={i}
+                index={i}
+                onDeleteComment={this.handleDeleteComment.bind(this)}
+                />)}</div>
         )
     }
 }
